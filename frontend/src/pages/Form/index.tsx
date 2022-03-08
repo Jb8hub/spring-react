9d60c2c84dc0f154acdac003a4ccf6a2b2acd2f7
Jb8hub <jb8git.hub@gmail.com>
2022-03-08 17:45:10 -0300

    MovieCard/Navigation buttons

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,4 +1,5 @@
-import './styles.css';
+import { Link } from "react-router-dom";
+import "./styles.css";
 
 function Form() {
   const movie = {
@@ -12,7 +13,11 @@ function Form() {
 
   return (
     <div className="dsmovie-form-container">
-      <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
+      <img
+        className="dsmovie-movie-card-image"
+        src={movie.image}
+        alt={movie.title}
+      />
       <div className="dsmovie-card-bottom-container">
         <h3>{movie.title}</h3>
         <form className="dsmovie-form">
@@ -35,8 +40,10 @@ function Form() {
               Salvar
             </button>
           </div>
-        </form>
-        <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+        </form> 
+        <Link to="/">
+          <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+        </Link>
       </div>
     </div>
   );
