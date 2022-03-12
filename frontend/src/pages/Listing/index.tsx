8c7425db57141b767ffa4e93a8c411c136006872
Jb8hub <jb8git.hub@gmail.com>
2022-03-12 11:57:04 -0300

    Show score

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -20,7 +20,7 @@ function Listing() {
   });
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`).then((resp) => {
+    axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`).then((resp) => {
       const data = resp.data as MoviePage;
       setPage(data);
     });
